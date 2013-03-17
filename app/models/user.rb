5216b6339f4205a392d79342a8860d029f474c4a
Akash Soti <akujgd@gmail.com>
2013-03-18 02:57:44 +0530

    fix password bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
 	def self.authenticate(email, password)
 		user = find_by_email(email)
-		if user && user.password == BCryt::Engine.hash_secret(password)
+		if user && user.password_digest == BCrypt::Engine.hash_secret(password)
 			user
 		end
 	end
